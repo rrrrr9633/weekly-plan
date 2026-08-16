c0b13f65506c2a340b0f4166deaaa3a4c42a944b
ChenHaoran <13464441403@proton.me>
2026-08-17 03:30:44 +0800

    1

--- src/main/java/com/weeklyplan/user/UpdateMyProfileRequest.java
+++ src/main/java/com/weeklyplan/user/UpdateMyProfileRequest.java
@@ -1,8 +1,9 @@
 package com.weeklyplan.user;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UpdateMyProfileRequest(
-    @NotBlank @Size(min = 2, max = 30) String displayName
+    @JsonAlias("username") @NotBlank @Size(min = 2, max = 30) String displayName
 ) {}
