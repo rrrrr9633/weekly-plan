5d573f0d3d0e0eb513e04b33f2dc77113bc52183
ChenHaoran <13464441403@proton.me>
2026-08-17 04:06:49 +0800

    1

--- target/classes/db/migration/V4__support_multiple_week_plans_and_weekdays.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE week_plans
-  ADD COLUMN weekday VARCHAR(16) NOT NULL DEFAULT 'PENDING';
-
-ALTER TABLE week_plans DROP INDEX uk_week_plans_user_project_week;
-
-CREATE INDEX idx_week_plans_user_project_week ON week_plans(user_id, project_id, year_number, week_number);
--- target/classes/db/migration/V6__backfill_user_display_names.sql
+++
@@ -1,3 +0,0 @@
-UPDATE users
-SET display_name = username
-WHERE display_name IS NULL OR TRIM(display_name) = '';
